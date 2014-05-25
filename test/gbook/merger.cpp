fa718767813409e69745d5be6c8ef31f4c5e1a18
Tomas Paladin Volf <paladin@jstation.cz>
2014-05-25 08:39:45 +0200

    added contacts mock for merger

--- test/gbook/merger.cpp
+++ test/gbook/merger.cpp
@@ -4,10 +4,9 @@ using namespace gbook;
 namespace gbook {
     class contactsMock {
     public:
-        // MOCK_METHOD1(add, void(user &));
-        // MOCK_METHOD1(update, void(user));
-        // MOCK_METHOD1(remove, void(string));
-        MOCK_METHOD0(add, void());
+        MOCK_METHOD1(add, void(user &));
+        MOCK_METHOD1(update, void(user));
+        MOCK_METHOD1(remove, void(string));
     };
 }
 
