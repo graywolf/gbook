142dc93ca19f05ab8d78b850847b4283e8a9a859
Tomas Paladin Volf <paladin@jstation.cz>
2014-05-31 23:18:21 +0200

    cleaned up gbook::sync. now is empty. will write it again

--- include/sync.h
+++ include/sync.h
@@ -19,77 +19,13 @@ namespace gbook {
          * Constructs sync instance and assure we have access token. Either authenticates user
          * or refreshes access token.
          **/
-        sync(std::string abook_folder) :
-            abook_folder_(abook_folder),
-            abook_data_file_(abook_folder + "/addressbook"),
-            gbook_sync_record_(abook_folder + "/gbook_sync_record"),
-            gbook_id_file_(abook_folder + "/gbook_id_file"),
-            max_id_(0LL),
-            o2_(abook_folder + "/o2_config")
-            {
-                if (o2_.access_token().empty()) {
-                    std::cout << "Access token is empty, contacting server..." << std::endl;
-                    user_data ud = o2_.request_user_code();
-                    std::cout << "Go to: " << ud.verification_url << std::endl;
-                    std::cout << "And enter code: " << ud.user_code << std::endl;
-                    o2_.begin_polling();
-                    std::cout << "Ok, we've got access token." << std::endl;
-                } else {
-                    std::cout << "Refreshing access token." << std::endl;
-                    o2_.refresh_access_token();
-                }
-            }
+        sync(std::string abook_folder);
         /**
          * Do the whole sync.
          **/
         void do_sync();
     private:
-        std::string abook_folder_;
-        std::string abook_data_file_;
-        std::string gbook_sync_record_;
-        std::string gbook_id_file_;
-
-        //google users indexed by google contact id
-        std::map<std::string, user> google_id_map_;
-        //abook users indexed by abook id
-        std::map<std::string, user> abook_id_map_;
-        //users as they were after last sync indexed by abook id
-        std::map<std::string, user> abook_id_map_from_last_sync_;
-        //set with users changed in abook, to be updated in google contacts
-        std::set<std::string> ids_changed_in_abook_;
-        long long max_id_;
-        //two maps mappinig abook ids to google ids and vice versa
-        std::map<std::string, std::string> abook_google_id_map_;
-        std::map<std::string, std::string> google_abook_id_map_;
-
         oauth2 o2_;
-
-        /**
-         * Loads abook data file.
-         **/
-        void load_abook();
-        /**
-         * Loads google contacts.
-         **/
-        void load_google();
-        /**
-         * Loads abook-google id mappings.
-         **/
-        void load_id_maps();
-
-        /**
-         * Handles changes in abook and pushes them into google.
-         **/
-        void abook_to_google();
-        /**
-         * Handles changes in google and pushes them into abook.
-         **/
-        void google_to_abook();
-
-        /**
-         * Save the resulting list of users into abook data file, saves id maps and cleans up.
-         **/
-        void wrap_it_up();
     };
 }
 
--- src/sync.cpp
+++ src/sync.cpp
@@ -2,6 +2,8 @@
 #include "user_reader.h"
 #include "user_writer.h"
 #include "contacts.h"
+#include "config.h"
+#include "macros.h"
 
 #include <iostream>
 #include <fstream>
@@ -9,188 +11,22 @@
 using namespace std;
 using namespace gbook;
 
+sync::sync(string abook_folder) {
+    if (config::get().access_token().empty()) {
+        LOG_DEBUG("Access token is empty, contacting server...");
+        user_data ud = o2_.request_user_code();
+        cout    << "To continue please go to "
+                << ud.verification_url << endl;
+                << "And enter the following code when prompted to: "
+                << ud.user_code
+                << endl;
+        o2_.begin_polling();
+        cout << "Ok, we've got access token." << endl;
+    } else {
+        LOG_DEBUG("Refreshing access token.");
+        o2_.refresh_access_token();
+    }
+}
+
 void sync::do_sync() {
-    load_id_maps();
-    load_abook();
-    load_google();
-
-    abook_to_google();
-    google_to_abook();
-
-    wrap_it_up();
-}
-
-void sync::load_id_maps() {
-    cout << "Loading gbook <--> abook mapping... ";
-    ifstream inf(gbook_id_file_);
-    string abook_id, google_id;
-    while (inf >> abook_id >> google_id) {
-        abook_google_id_map_[abook_id] = google_id;
-        google_abook_id_map_[google_id] = abook_id;
-
-        long long id = stoll(abook_id);
-        if (id > max_id_) {
-            max_id_ = id;
-        }
-    }
-    cout << "Loaded." << endl;
-}
-
-void sync::load_abook() {
-    cout << "Loading abook... ";
-    ifstream inf(abook_data_file_);
-    vector<user> abook_users = load_users_from_stream(inf);
-    for (user & u : abook_users) {
-        if (u.custom5.empty()) {
-            u.custom5 = to_string(++max_id_);
-        }
-        abook_id_map_[u.custom5] = u;
-    }
-    cout << "Loaded." << endl;
-
-    cout << "Loading records of last sync... ";
-    ifstream inf_sync_record(gbook_sync_record_);
-    vector<user> last_synced_users = load_users_from_stream(inf_sync_record);
-    for (user & u : last_synced_users) {
-        abook_id_map_from_last_sync_[u.custom5] = u;
-    }
-    cout << "Loaded." << endl;
-}
-
-void sync::load_google() {
-    cout << "Loading contacts from google... ";
-    contacts cs(o2_);
-    vector<user> google_users = cs.get_all();
-    for (user & u : google_users) {
-        google_id_map_[u.get_id("google")] = u;
-    }
-    cout << "Loaded." << endl;
-}
-
-void sync::abook_to_google() {
-    cout << "Sync abook --> google..." << endl;
-    contacts cs(o2_);
-    //let's find out deleted
-    cout << "Deleting deleted..." << endl;
-    for (auto & it : abook_id_map_from_last_sync_) {
-        if (abook_id_map_.find(it.first) == abook_id_map_.end()) {
-            try {
-                cout << "Removing " << it.second.name << " from google." << endl;
-                ids_changed_in_abook_.insert(it.second.custom5);
-                cs.remove(abook_google_id_map_[it.second.custom5]);
-                //remove it from two-way map
-                google_id_map_.erase(abook_google_id_map_[it.second.custom5]);
-                google_abook_id_map_.erase(abook_google_id_map_[it.second.custom5]);
-                abook_google_id_map_.erase(it.second.custom5);
-            } catch (runtime_error re) {
-                cerr << "Cannot delete: " << re.what() << endl;
-            }
-        }
-    }
-    cout << "Done." << endl;
-
-    //let's find out new
-    cout << "Adding new..." << endl;
-    for (auto & it : abook_id_map_) {
-        if (abook_id_map_from_last_sync_.find(it.first) == abook_id_map_from_last_sync_.end()) {
-            try {
-                cout << "Adding " << it.second.name << " to google." << endl;
-                cs.add(it.second);
-                //add it to two-way map
-                google_id_map_[it.second.get_id("google")] = it.second;
-                abook_google_id_map_[it.second.custom5] = it.second.get_id("google");
-                google_abook_id_map_[it.second.get_id("google")] = it.second.custom5;
-            } catch (runtime_error re) {
-                cerr << "Cannot add: " << re.what() << endl;
-            }
-        }
-    }
-    cout << "Done." << endl;
-
-    //let's find out updated
-    cout << "Updating updated..." << endl;
-    for (auto & it : abook_id_map_) {
-        //if existed in last sync
-        if (abook_id_map_from_last_sync_.find(it.first) != abook_id_map_from_last_sync_.end()) {
-            //check if it changed
-            if (it.second != abook_id_map_from_last_sync_.find(it.first)->second) {
-                try {
-                    cout << "Updating " << it.second.name << " in google." << endl;
-                    it.second.set_id("google", abook_google_id_map_[it.second.custom5]);
-                    ids_changed_in_abook_.insert(it.second.custom5);
-                    cs.update(it.second);
-                } catch (runtime_error re) {
-                    cerr << "Cannot update: " << re.what() << endl;
-                }
-            }
-        }
-    }
-    cout << "Done." << endl;
-}
-
-void sync::google_to_abook() {
-    cout << "Sync abook <-- google..." << endl;
-    contacts cs(o2_);
-
-    //let's find out deleted
-    cout << "Deleting deleted..." << endl;
-    for (auto & it : google_abook_id_map_) {
-        //there is deleted
-        if (google_id_map_.find(it.first) == google_id_map_.end()) {
-            //delete from abook
-            if (abook_id_map_.find(it.second) != abook_id_map_.end()) {
-                cout << "Deleting " << abook_id_map_[it.second].name << " from abook." << endl;
-                abook_google_id_map_.erase(it.second);
-                abook_id_map_.erase(it.second);
-            }
-        }
-    }
-
-    cout << "Processing google contacts..." << endl;
-    for (auto & it : google_id_map_) {
-        //this is new contact from google
-        if (google_abook_id_map_.find(it.second.get_id("google")) == google_abook_id_map_.end()) {
-            cout << "Adding " << it.second.name << " to abook." << endl;
-            it.second.custom5 = to_string(++max_id_);
-            //push custom5 upstream
-            cs.update(it.second);
-            abook_id_map_[it.second.custom5] = it.second;
-            //add it to two-way map
-            abook_google_id_map_[it.second.custom5] = it.second.get_id("google");
-            google_abook_id_map_[it.second.get_id("google")] = it.second.custom5;
-        } else {
-        //this contact is already in the db, test if there's need for update
-            user & abook_user = abook_id_map_[google_abook_id_map_.find(it.second.get_id("google"))->second];
-            if (abook_user != it.second && ids_changed_in_abook_.count(it.second.custom5) == 0) {
-                cout << "Updating " << it.second.name << " in abook." << endl;
-                abook_user = it.second;
-            }
-        }
-    }
-    cout << "Done." << endl;
-}
-
-void sync::wrap_it_up() {
-    cout << "Updating files..." << endl;
-
-    cout << "Saving new abook addressbook... ";
-    vector<user> abook_users;
-    for (auto & it : abook_id_map_) {
-        abook_users.push_back(it.second);
-    }
-    ofstream abook_data_file(abook_data_file_);
-    abook_data_file << abook_users;
-    cout << "Done." << endl;
-
-    cout << "Saving sync record... ";
-    ofstream gbook_sync_record_file(gbook_sync_record_);
-    gbook_sync_record_file << abook_users;
-    cout << "Done." << endl;
-
-    cout << "Saving abook <--> google mapping... ";
-    ofstream gbook_id_file(gbook_id_file_);
-    for (auto & it : abook_google_id_map_) {
-        gbook_id_file << it.first << " " << it.second << endl;
-    }
-    cout << "Done." << endl;
 }
